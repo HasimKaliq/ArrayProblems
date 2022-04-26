f027603954b5b52fc810cf2cc0e4279a59788913
HasimKaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-04-26 00:33:45 -0700

    Add files via upload

---
+++ Subarray with given sum.py
@@ -0,0 +1,43 @@
+# Given an unsorted array A of size N 
+# that contains only non-negative integers, 
+# find a continuous sub-array which adds to a given number S.
+
+# In case of multiple subarrays, 
+# return the subarray which comes first on moving from left to right.
+
+
+# Example 1:
+
+# Input:
+# N = 5, S = 12
+# A[] = {1,2,3,7,5}
+# Output: 2 4
+# Explanation: The sum of elements 
+# from 2nd position to 4th position 
+# is 12.
+
+
+# Example 2:
+
+# Input:
+# N = 10, S = 15
+# A[] = {1,2,3,4,5,6,7,8,9,10}
+# Output: 1 5
+# Explanation: The sum of elements 
+# from 1st position to 5th position
+# is 15.
+
+
+# Expected Time Complexity: O(N)
+# Expected Auxiliary Space: O(1)
+
+
+arr = [1,2,3,7,5]
+arr = [1,2,3,4,5,6,7,8,9,10]
+for othIndexPointer in range(0, len(arr)):
+    othIndexPointerValue = arr[othIndexPointer]
+    secondIndexPointer = arr[len(arr) - 1]
+    sumTotalOfAllPointers = (othIndexPointerValue  + secondIndexPointer)
+    if(sumTotalOfAllPointers == 15):
+        print(othIndexPointer)
+        print(arr.index(secondIndexPointer))
